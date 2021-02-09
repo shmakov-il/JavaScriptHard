b46c6ee7816d54bd1c175be084341e9a900d5552
Shmakov Ilya <ilia.eduardovich98@gmail.com>
2021-02-09 17:59:52 +0300

    feat(main.js): create function

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
 'use strict';
 
-// Первая задача
-const lang = prompt('Введите "en" или "ru"');
-// С помощью if
-if (lang === 'en') {
-  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-} else if (lang === 'ru') {
-  console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+let isNumber = function(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+let text = prompt('Напишите что-нибудь...');
+
+function myFunction (text) {
+  // проверяем на строка/не строка
+  if (isNumber(text)) {
+    alert('Напишите строку!');
+  } 
+
+  // убираем пробелы в начале и конце строки
+  text = text.trim();
+
+  // вводим ограниченеие по символам
+  if (text.length > 30) {
+    return text.slice(0, 30) + '...';
+  } else {
+    return text;
+  } 
 }
 
-// С помощью switch
-switch (lang) {
-  case 'en':
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-    break;
-  case 'ru':
-    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-    break;
-}
+alert(myFunction(text));
 
-// С помощью многомерного массива
-const week = [];
-week['ru'] = ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'];
-week['en'] = ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'];
-console.log(String(week[lang]));
-
-// Вторая задача
-const namePerson = prompt('Введите имя');
-
-const result = namePerson === 'Артем' ? console.log('Директор') :
-  namePerson === 'Максим' ? console.log('Преподаватель') :
-  console.log('Студент');
