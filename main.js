beca43616bf4ebb96af91eaf14cd5b59e9298221
Shmakov Ilya <ilia.eduardovich98@gmail.com>
2021-02-22 14:39:46 +0300

    fix(main.js): add while

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
 'use strict';
 
-// Первая задача
-const arr = ['593121', '24421', '290032', '465', '357', '112', '422332',];
-
-arr.forEach(function(number) {
-  if (number.startsWith('2') || number.startsWith('4')) {
-    console.log(number);
+let text;
+function myFunction (text) {
+  text = prompt('Напишите что-нибудь...');
+  while (!text.trim() || /[0-9]/.test(text)) {
+    alert('Введите строку!');
+    text = prompt('Напишите что-нибудь...');
   }
-});
+  text = text.trim();
 
-// Вторая задача
-// Создаем вложенный цикл с меткой
-nextNumber:
-for (let i = 2; i <= 100; i++) { 
-  for (let j = 2; j < i; j++) { 
-    if (i % j === 0) {
-      continue nextNumber; 
-    } 
-  }
-  console.log( i + '. Делители этого числа ' + 1 + ' и ' + i );
+  if (text.length > 30) {
+    return text.slice(0, 30) + '...';
+  } else {
+    return text;
+  } 
 }
+console.log(myFunction(text));
