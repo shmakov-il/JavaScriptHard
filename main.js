02ce32bc7c2d4ffd5ed65019aaa1bc54be4edec0
Shmakov Ilya <ilia.eduardovich98@gmail.com>
2021-03-01 13:41:37 +0300

    feat(main.js): create 14lesson hard f
    git push

--- main.js
+++ main.js
@@ -1,21 +1,53 @@
-'use strict';
+window.addEventListener('DOMContentLoaded', () => {
 
-const title = document.getElementById('color'),
-  btn = document.getElementById('change');
+  'use strict';
 
-const random = (x, y) => {
-  return Math.round(Math.random() * (y - x) + x);
-};
+  function DomElement(height = '250px', width = '250px', bg = 'blue', position = 'absolute') {
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.position = position;
+  }
 
-const randomColor = () => {
-  let red = random(0, 255).toString(16);
-  let green = random(0, 255).toString(16);
-  let blue = random(0, 255).toString(16);
-  return '#' + red + green + blue;
-};
+  DomElement.prototype.createElement = function () {
 
-btn.addEventListener('click', () => {
-  const color = randomColor();
-  document.body.setAttribute('style', `background-color: ${color}`);
-  title.textContent = color;
+    let element = document.createElement('div');
+
+    const createElement = () => {
+      document.body.append(element);
+      element.classList.add('block'); 
+    };
+
+    createElement(); 
+    
+    DomElement.prototype.cssText = function () {
+      let styleForElement = document.querySelector('.block');
+      styleForElement.style.cssText = `
+      height: ${this.height};
+      width: ${this.width};
+      background-color: ${this.bg};
+      position: ${this.position};
+      `;
+    };
+  };
+    
+  const newElement = new DomElement ('100px', '100px', 'tomato');
+  newElement.createElement();
+  newElement.cssText();
+
+  window.addEventListener('keydown', event => {
+    const element = document.querySelector('.block');
+    const styleElement = getComputedStyle(element);
+    console.log(event.key);
+    if (event.key === 'ArrowRight') {
+      element.style.left = parseInt(styleElement.left) + 10 + 'px';
+    } else if (event.key === 'ArrowLeft') {
+      element.style.left = parseInt(styleElement.left) - 10 + 'px';
+    } else if (event.key === 'ArrowUp') {
+      element.style.top = parseInt(styleElement.top) - 10 + 'px';
+    } else if (event.key === 'ArrowDown') {
+      element.style.top = parseInt(styleElement.top) + 10 + 'px';
+    }
+  });
+
 });
